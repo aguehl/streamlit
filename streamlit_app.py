6b7a58f8f920533fbf7b28d39e1f903816b888b3
aguehl <31312976+aguehl@users.noreply.github.com>
2022-03-29 15:01:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 st.title('Hello Wilders, welcome to my application!')
+st.write("I enjoy to discover stremalit possibilities")
 
-name = st.text_input("Please give me your name :")
-name_length = len(name)
-st.write("Your name has ",name_length,"characters")
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
+df_weather = pd.read_csv(link)
 
-st.text('This is some text.')
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+st.line_chart(df_weather)
 
-st.line_chart(chart_data)
+
+viz_correlation = sns.heatmap(df_weather.corr(), 
+								center=0,
+								cmap = sns.color_palette("vlag", as_cmap=True)
+								)
+
+st.pyplot(viz_correlation.figure)
 
