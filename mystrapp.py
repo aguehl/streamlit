b8dec3a2cd045edb4b12503a2e2dd79437a5ce23
aguehl <31312976+aguehl@users.noreply.github.com>
2022-03-29 18:51:59 +0200

    Update mystrapp.py

--- mystrapp.py
+++ mystrapp.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import plotly.graph_objects as go
+
 st.title("Hello  Wilder")
 
 link = 'https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv'
@@ -12,5 +14,18 @@ select = st.sidebar.multiselect('Which region do you want?',options, default= op
 filtered_df = df_cars[df_cars['continent'].isin(select)]
 st.write(filtered_df)
 
-chart_df = df_cars[df_cars['wheightlbs'], df_cars['time-to-60']
-st.line_chart(chart_df)
+
+poids = df_cars['wheightlbs'].tolist()
+temps = df_cars['time-to-60'].tolist()
+
+#The plot
+fig = go.Figure(
+    go.Pie(
+    labels = poids,
+    values = temps,
+    hoverinfo = "label+percent",
+    textinfo = "value"
+))
+
+st.header("Pie chart")
+st.plotly_chart(fig)
