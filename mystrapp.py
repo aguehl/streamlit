f673101066c8201a6a99b0ac314d5f65e4ed77b4
aguehl <31312976+aguehl@users.noreply.github.com>
2022-03-29 17:30:45 +0200

    Update mystrapp.py

--- mystrapp.py
+++ mystrapp.py
@@ -8,6 +8,6 @@ df_cars = pd.read_csv(link)
 
 
 options = df_cars['continent'].unique().tolist()
-select = st.sidebar.multiselect('Which region do you want?',options, default= option)
+select = st.sidebar.multiselect('Which region do you want?',options, default= options)
 filtered_df = df_cars[df_cars['continent'].isin(select)]
 st.write(filtered_df)
