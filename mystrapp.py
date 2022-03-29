154fae57a3c198b873bde3416e8edc336d0ebf9b
aguehl <31312976+aguehl@users.noreply.github.com>
2022-03-29 17:22:30 +0200

    Update mystrapp.py

--- mystrapp.py
+++ mystrapp.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
+st.title("Hello")
 
 link = 'https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv'
 df_cars = pd.read_csv(link)
 
-st.write(df_cars)
+
+options = df_cars['continent'].unique().tolist()
+select = st.sidebar.multiselect('Which region do you want?',options)
+
+st.write(df_cars, select)
