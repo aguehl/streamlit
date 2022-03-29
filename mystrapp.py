8ee72f9edef527635bd809ce6733839da8ee5981
aguehl <31312976+aguehl@users.noreply.github.com>
2022-03-29 18:37:04 +0200

    Update mystrapp.py

--- mystrapp.py
+++ mystrapp.py
@@ -5,9 +5,10 @@ st.title("Hello  Wilder")
 link = 'https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv'
 df_cars = pd.read_csv(link)
 
-
+st.write(df_cars)
 
 options = df_cars['continent'].unique().tolist()
 select = st.sidebar.multiselect('Which region do you want?',options, default= options)
 filtered_df = df_cars[df_cars['continent'].isin(select)]
 st.write(filtered_df)
+
