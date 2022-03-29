cee55b972a21e7e2a1c1f5579bd3972eabac6386
aguehl <31312976+aguehl@users.noreply.github.com>
2022-03-29 15:25:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Hello Wilders, welcome to my application!')
-st.write("I enjoy to discover stremalit possibilities")
+rad = st.radio('1','2')
+
+if  rad == '1':
+  st.title('Hello Wilders 1')
+if  rad == '2':
+  st.title('Hello Wilders 2')
+
+slide = st.slider('Taille du titre', 0, 100, 50, 10)
 
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
 df_cars = pd.read_csv(link)
