c9c1ac435bcfcb062a6f63f87f6c6e45c0f6dd82
aguehl <31312976+aguehl@users.noreply.github.com>
2022-03-30 11:52:43 +0200

    Update mystrapp.py

--- mystrapp.py
+++ mystrapp.py
@@ -17,17 +17,22 @@ filtered_df = df_cars[df_cars['continent'].isin(select)]
 st.write(filtered_df)
 
 viz_correlation = sns.heatmap(df_cars.corr(), 
-								center=0,
-								cmap = sns.color_palette("vlag", as_cmap=True)
-								)
+			center=0,
+			annot= True,
+			cmap = sns.color_palette("vlag", as_cmap=True)
+			)
+St.title('Correlation entre les éléments'
 st.pyplot(viz_correlation.figure)
 
 st.balloons()
+ 
 
+horse_power = df_cars['hp']
+temps = df_cars['time-to-60']
 
-# horse_power = df_cars['hp']
-# temps = df_cars['time-to-60']
+Cor= np.corcoef(horse_power,temps)
 
+	 
 fig = px.scatter(df_cars, y='time-to-60', x='hp', trendline="ols",  color="time-to-60", color_continuous_scale="turbo",
                 labels={
                   'time-to-60':'Temps de 0 à 60mph',
@@ -36,6 +41,7 @@ fig = px.scatter(df_cars, y='time-to-60', x='hp', trendline="ols",  color="time-
 
 
 st.plotly_chart(fig, use_container_width=True)
+st.subheader('Coef de correlation", Cor)
 
 import seaborn as sns
 
