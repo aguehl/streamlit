79f8ce4de5c01c2ccfef63e8c3e30d62ade970df
aguehl <31312976+aguehl@users.noreply.github.com>
2022-03-30 10:12:04 +0200

    Update mystrapp.py

--- mystrapp.py
+++ mystrapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objects as go
-import plotly.figure_factory  as ff
+import plotly.express as px
+
 st.title("Hello  Wilder")
 
 link = 'https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv'
@@ -15,19 +15,12 @@ filtered_df = df_cars[df_cars['continent'].isin(select)]
 st.write(filtered_df)
 
 
-poids = df_cars['weightlbs'].tolist()
-temps = df_cars['time-to-60'].tolist()
+poids = df_cars['weightlbs']
+temps = df_cars['time-to-60']
 
-graph_data = [poids, temps]
-graph_labels = ['Wheight', 'Time to 60mph']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-         graph_data, graph_labels, bin_size=[.1, .25, .5])
+fig = px.scatter(poids, temps)
+fig.show()
 
 
-
-st.header("Time to 60mph by the wheight ")
-st.plotly_chart(fig, use_container_width=True)
-
+# st.plotly_chart(fig, use_container_width=True)
 
