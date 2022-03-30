3537b8df092f53429286cc8f8f625e8fc91dd29a
aguehl <31312976+aguehl@users.noreply.github.com>
2022-03-30 11:37:31 +0200

    Update mystrapp.py

--- mystrapp.py
+++ mystrapp.py
@@ -16,11 +16,17 @@ select = st.sidebar.multiselect('Which region do you want?',options, default= op
 filtered_df = df_cars[df_cars['continent'].isin(select)]
 st.write(filtered_df)
 
+st.balloons()
+
 
 # horse_power = df_cars['hp']
 # temps = df_cars['time-to-60']
 
-fig = px.scatter(df_cars, y='time-to-60', x='hp', trendline="ols")
+fig = px.scatter(df_cars, y='time-to-60', x='hp', trendline="ols",  color="time-to-60", color_continuous_scale="turbo",
+                labels={
+                  'time-to-60':'Temps de 0 à 60mph',
+                  'hp':'Horse Power'},
+                title='Correlation entre la puissance et le temps pour arriver à 60mph)
 
 
 st.plotly_chart(fig, use_container_width=True)
